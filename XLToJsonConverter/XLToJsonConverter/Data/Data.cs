4961e67e7955abf56b1b1a757dc4f76fce4d4899
m5623skhj <m5623skhj@gmail.com>
2023-09-03 19:24:07 +0900

    * 데이터 파일 수정 및 테스트 파일 추가

--- XLToJsonConverter/XLToJsonConverter/Data/Data.cs
+++ XLToJsonConverter/XLToJsonConverter/Data/Data.cs
@@ -1,8 +1,36 @@
-﻿namespace Data
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data
 {
     public struct Test
     {
         public int? id;
         public string stringItem;
+    }
+
+    public struct Test2
+    {
+        public struct Ids
+        {
+            public int? id1;
+            public int? id2;
+        }
+
+        public Ids ids;
+        public List<string> arrayType1;
+        public List<Ids> arrayType2;
+    }
+
+    public struct Test3
+    {
+        public struct Temp
+        {
+            public int? t1;
+            public string t2;
+        }
+
+        public Temp temp;
+        public int? t3;
     }
 }
